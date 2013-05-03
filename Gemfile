2ca640a21921e43b1f208923aae612f9fbbfb4de
Gabriel Osorio <gabriel.osorio.montelius@gmail.com>
2013-05-03 00:40:19 -0300

    Added Mongoid to save Sayings and created a random roulette display.

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ ruby '1.9.3'
 
 gem 'sinatra'
 gem 'haml'
+gem 'mongo'
+gem 'mongoid'
--- main.rb
+++ main.rb
@@ -1,9 +1,41 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'mongoid'
+
+configure do
+  Mongoid.configure do |config|
+    config.sessions = { :default => { :hosts => ["localhost:27017"], :database => "saywat" } }
+  end
+end
+
+class Saying
+  include Mongoid::Document
+  field :wat
+  field :who
+  field :wen, type: Time, default: Time.now
+end
 
 get '/' do
   haml :index
+end
+
+get '/list' do
+  Saying.all.to_s
+end
+
+get '/roulette' do
+  haml :roulette, locals: { saying: Saying.all.sample }
+end
+
+post '/spread-the-word' do
+  return "Don't be leaving empty params..." if params["wat"].empty? || params["who"].empty?
+
+  if Saying.create(wat: params["wat"], who: params["who"])
+    redirect back
+  else
+    "Does this unformatted text make you feel miserable enough to understand that this is an error?"
+  end
 end
 
 __END__
@@ -13,12 +45,15 @@ __END__
   %head
     <link href='http://fonts.googleapis.com/css?family=Playfair+Display:400,900' rel='stylesheet' type='text/css'>
     :css
+      body { overflow: hidden }
       * { font-family: 'Playfair Display', serif; font-weight: bold; }
-      body { display: -webkit-box; -webkit-box-pack: center; -webkit-box-orient: horizontal; -webkit-box-align: center; } display: -moz-box; -moz-box-pack: center; -moz-box-orient: horizontal; -moz-box-align: center; display: box; box-pack: center; box-orient: horizontal; box-align: center; }
-      #main-wrapper { width: 800px; -webkit-box-flex: 1; }
+      #main-wrapper { height: 100%; width: 100%; display: -webkit-box; -webkit-box-pack: center; -webkit-box-orient: horizontal; -webkit-box-align: center; display: -moz-box; -moz-box-pack: center; -moz-box-orient: horizontal; -moz-box-align: center; display: box; box-pack: center; box-orient: horizontal; box-align: center; }
+      #main-wrapper.black { background: black; color: white; }
+      #content { padding: 0 150px; -webkit-box-flex: 1; -moz-box-flex: 1; box-flex: 1; }
       h1, h2 { text-align: center; text-shadow: 1px 1px rgba(255, 165, 115, 0.5); }
       h1 { font-size: 78px; }
       h2 { font-size: 62px; }
+      h2.author { font-size: 50px; text-align: right; }
       .field { width: 400px; margin: auto; }
       input, input:focus { border: none; outline: none; width: 100%; background: black; color: white; padding: 20px; font-size: 28px; border-radius: 26px; box-shadow: 1px 1px rgba(255, 165, 115, 0.5); }
       input[type=submit] { visibility: hidden; }
@@ -28,15 +63,27 @@ __END__
 
 @@ index
 #main-wrapper
-  %form{ action: '/spread_the_word', method: 'POST' }
-    %h1
-      <span class="even">Say</span>Wat?
-    .field
-      %input{ type: 'text', name: 'say_what' }
+  #content
+    %form{ action: '/spread-the-word', method: 'POST' }
+      %h1 SayWat?
+      .field
+        %input{ type: 'text', name: 'wat' }
+
+      %h2 SezWho?
+      .field
+        %input{ type: 'text', name: 'who' }
+
+      %input{ type: "submit", value: "Spread the Word" }
+
+@@ roulette
+#main-wrapper.black
+  #content
+    %h1 SayWat?
 
     %h2
-      Sez<span class="even">Who?</span>
-    .field
-      %input{ type: 'text', name: 'says_who' }
-    
-    %input{ type: "submit", value: "Spread the Word" }
+      &#8220;
+      = saying.wat
+      &#8221;
+    %h2.author
+      &#8212;
+      = saying.who
